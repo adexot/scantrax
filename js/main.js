c6e50bd7d92ff3a852e1623cd014cf845bffb5f0
kunle <adekunle.oseni@konga.com>
2019-04-08 03:34:26 +0100

    fix mobile nav

--- js/main.js
+++ js/main.js
@@ -4,7 +4,15 @@ const getElement = (element) => document.querySelector(element);
 window.addEventListener('load', () => {
     log('**** Welcome to ScanTranx ****');
 
-    const mobileNavToggle = getElement('#mobile-nav-toggle');
+    const mobileNavButton = getElement('#mobile-nav-button');
 
-    log(mobileNavToggle);
+    let isNavActive = false;
+    mobileNavButton.addEventListener('click', () => {
+        if (!isNavActive) {
+            mobileNavButton.classList.add('active');
+        } else {
+            mobileNavButton.classList.remove('active');
+        }
+        isNavActive = !isNavActive;
+    });
 });
